0303aa807492d6b8e457cf920bf66a8084e49c53
Georgii Mukha <georgymukha@yandex.ru>
2022-11-06 17:33:43 +0600

    Repo init

---
+++ js/main.js
@@ -0,0 +1,96 @@
+$(".country__button").on("click", function (event) {
+  event.preventDefault();
+
+  $(".loader__bg").addClass("is-active");
+
+  let countryName = $(".country__input").val();
+  if (countryName !== "") {
+    clear();
+    findCountry(countryName);
+  } else {
+    alert("Enter name of country, please");
+    $(".loader__bg").removeClass("is-active");
+  }
+});
+
+let clear = function () {
+  $(".table-country").addClass("loading");
+  $(".country__input").val("");
+};
+
+let findCountry = function (countryName) {
+  axios
+    .get(`https://restcountries.com/v3.1/name/${countryName}`)
+    .then((response) => {
+      $(".loader__bg").addClass("is-active");
+      response = response.data[0];
+      $(".country-flag").html(`<img
+      class="img-fluid img-thumbnail"
+      src="${response.flags.svg}"
+      alt=""
+    />
+    <div
+                class="card-body d-flex align-items-center justify-content-center"
+              >
+                <h5 class="card-title text-secondary p-1">Flag</h5>
+              </div>
+    `);
+      $(".country-coat-of-arms").html(`<img
+      class="img-thumbnail"
+      src="${response.coatOfArms.svg}"
+      alt=""
+    />
+    <div
+      class="card-body d-flex align-items-center justify-content-center"
+    >
+      <h5 class="card-title text-secondary">Coat of arms</h5>
+    </div>
+    `);
+      $(".country-name").text(`${response.name.official}`);
+      $(".country-capital").text(`${response.capital}`);
+      $(".country-currency").text(
+        `${Object.keys(response.currencies).join(", ")}`
+      );
+      $(".country-languages").text(
+        `${Object.values(response.languages).join(", ")}`
+      );
+
+      response.borders.map((border) => {
+        axios
+          .get(`https://restcountries.com/v3.1/alpha/${border}`)
+          .then(async (response) => {
+            response = response.data[0];
+            await makeBorders(response.name.official, response.flags.svg);
+          });
+      });
+    })
+    .then(async () => {
+      await $(".loader__bg").removeClass("is-active");
+      await $(".table-country").removeClass("loading");
+    })
+    .catch(async function () {
+      await alert(`Country "${countryName}" not found`);
+      await $(".loader__bg").removeClass("is-active");
+    });
+};
+
+let makeBorders = function (countryName, flag) {
+  $(".borders").append(`
+  <div class="col-2">
+  <div
+    class="h-100 border-country card justify-content-evenly align-items-center"
+  >
+    <img
+      class="img-fluid img-thumbnail"
+      src="${flag}"
+      alt=""
+    />
+    <div
+      class="card-body d-flex align-items-center justify-content-center"
+    >
+      <h5 class="card-title text-secondary p-1 text-center">${countryName}</h5>
+    </div>
+  </div>
+  </div>
+  `);
+};
